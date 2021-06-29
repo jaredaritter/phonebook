25bd85c83bc4d1a704683797e0669b2a7b3de4e6
jared <38332939+jaredaritter@users.noreply.github.com>
2021-06-29 18:10:04 -0400

    uibuild

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', function getBody(req) {
   return JSON.stringify(req.body);
 });
-
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
