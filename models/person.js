fa0cb17ce66b530d5842417ac75e215a37809d4d
jared <38332939+jaredaritter@users.noreply.github.com>
2021-07-09 16:21:57 -0400

    Added validator to ensure post name is unique.

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 // --------------------------------------
 // MONGODB CONNECTION
@@ -25,9 +26,16 @@ mongoose
 // MONGOOSE SCHEMA AND MODEL
 // --------------------------------------
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+  },
 });
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
