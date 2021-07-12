721fa6bdcc97c2882fd406e8320570aed2a80a0f
jared <38332939+jaredaritter@users.noreply.github.com>
2021-07-12 15:06:57 -0400

    Add eslint directly to repo for practice.

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/api/persons', (req, res, next) => {
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then((result) => {
+    .then(() => {
       res.status(204).end();
     })
     .catch((error) => next(error));
